70bfb09611efdf2dfda3b63f7d51387432cba49f
pooja062000 <vikaskosli9890@gmail.com>
2023-06-30 22:52:26 +0530

    new changes

--- src/Condition.jsx
+++ src/Condition.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+//import mylogo from '../src/Assets/Images/logo.jpg'
 
 class Condition extends React.Component {
     constructor(props) {
@@ -25,18 +26,18 @@ if (isloggedin) {
 
         <>
            <div className="from-outer">
-            <h2 className='lo'>Login</h2>
-            <div className="from">
-            <input type="text" placeholder="Full Name" className="input"/>
+            <div className='inst'></div>
+
+            <form className='hello'>                
+            <input type="text" placeholder="Username, phone number, or email" className="input"/>
+         <input type="password" placeholder="Password" className="input"/>
+
+            </form>
             
-            <input type="phone" placeholder="Phone" className="input2"/>
-            <input type="password" placeholder="Password" className="input2"/>
-
-        </div>
-
         <button onClick={this.login} className="logi">Login</button>
-        <h6>Developed by Pooja Mittal</h6>
         </div>
+
+        
         
         </>
     )
@@ -45,20 +46,17 @@ if (isloggedin) {
 
         <>
         <div className="from-outer">
-            <h2 className='lo'>Logout</h2>
-            <div className="from">
-            <input type="text" placeholder="Pooja" className="input"/>
             
-            <input type="password" placeholder="pooja@123" className="input2"/>
-            
-            <input type="phone" placeholder="9898776678" className="input2"/>
+        <div className='inst'></div>
+
+        <h3>User is logged in</h3>
+        
+     <button onClick={this.logout} className="logu">Logout</button>
 
         </div>
         
-     <button onClick={this.logout} className="logi">Logout</button>
 
-     <h6>Developed by Pooja Mittal</h6>
-        </div>
+     
         
         </>
     )
